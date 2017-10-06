1778d6615b06a97f00629a54cc8dae0529bb2a78
FilipElen <filipelengit@outlook.com>
2017-10-06 11:13:54 +0200

    Adds IOutputter and implements in ConsoleOutputter.cs REAL COMMIT IGNORE PREVIOUS

---
+++ C#/Trivia/Trivia/ConsoleOutputter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class ConsoleOutputter : IOutputter
+    {
+        public void GenerateOutput(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ C#/Trivia/Trivia/IOutputter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    public interface IOutputter
+    {
+        void GenerateOutput(string message);
+    }
+}
